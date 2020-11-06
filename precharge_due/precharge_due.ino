825699bc7f70f963fba71416776606b00a557bc5
Ryan Pope <poperyan73@gmail.com>
2020-11-05 18:09:41 -0600

    [Nick] Add new auxilary relay to precharge

--- precharge_due/precharge_due.ino
+++ precharge_due/precharge_due.ino
@@ -7,6 +7,7 @@ extern byte canRx(byte cPort, long* lMsgID, bool* bExtendedFormat, byte* cData, 
 
 const int PRECHARGE_RELAY_PIN = 2;
 const int SOURCE_RELAY_PIN = 3;
+const int AUX_RELAY_PIN = 4;
 const int CAN_THRESHOLD = 3 * 1000;
 
 void setup()
@@ -19,17 +20,21 @@ void setup()
 
   pinMode(SOURCE_RELAY_PIN, OUTPUT);
   digitalWrite(SOURCE_RELAY_PIN, HIGH);
-  
+
+
+  pinMode(AUX_RELAY_PIN, OUTPUT);
+  digitalWrite(AUX_RELAY_PIN, HIGH);
+
   // Initialize both CAN controllers
   if(canInit(0, CAN_BPS_250K) == CAN_OK)
     Serial.print("CAN0: Initialized Successfully.\n\r");
   else
     Serial.print("CAN0: Initialization Failed.\n\r");
-  
+
 }// end setup
 
 void loop()
-{  
+{
   int timer = 0;
   int lastCanFrame = 0;
 
@@ -48,19 +53,33 @@ void loop()
 
     if(canRx(0, &lMsgID, &bExtendedFormat, &cRxData[0], &cDataLen) == CAN_OK)
     {
-      if (lMsgID == 0x6B5) 
+      if (lMsgID == 0x6B5)
       {
         lastCanFrame = timer;
         byte relayState = cRxData[0];
 
-        if (relayState & 1) 
+        if (relayState & 1)
         {
-          if (timer >= 7000)
+          if (timer >= 8000)
           {
             digitalWrite(SOURCE_RELAY_PIN, LOW);
             Serial.print("Set source low\n");
             digitalWrite(PRECHARGE_RELAY_PIN, LOW);
             Serial.print("Set precharge low\n");
+            digitalWrite(AUX_RELAY_PIN, HIGH);
+            Serial.print("Set auxiliary relay high\n");
+            if (canTx(0, 0x6B4, false, cTxOff, 1) != CAN_OK) {
+              Serial.print("Error sending message\n");
+            }
+          }
+          else if (timer >= 7000)
+          {
+            digitalWrite(SOURCE_RELAY_PIN, LOW);
+            Serial.print("Set source low\n");
+            digitalWrite(PRECHARGE_RELAY_PIN, LOW);
+            Serial.print("Set precharge low\n");
+            digitalWrite(AUX_RELAY_PIN, LOW);
+             Serial.print("Set auxiliary relay low\n");
             if (canTx(0, 0x6B4, false, cTxOff, 1) != CAN_OK) {
               Serial.print("Error sending message\n");
             }
@@ -71,6 +90,8 @@ void loop()
             Serial.print("Set source high\n");
             digitalWrite(PRECHARGE_RELAY_PIN, LOW);
             Serial.print("Set precharge low\n");
+            digitalWrite(AUX_RELAY_PIN, LOW);
+             Serial.print("Set auxiliary relay low\n");
             if (canTx(0, 0x6B4, false, cTxOff, 1) != CAN_OK) {
               Serial.print("Error sending message\n");
             }
@@ -81,6 +102,8 @@ void loop()
             Serial.print("Set source high\n");
             digitalWrite(PRECHARGE_RELAY_PIN, LOW);
             Serial.print("Set precharge low\n");
+            digitalWrite(AUX_RELAY_PIN, HIGH);
+            Serial.print("Set auxiliary relay high\n");
             if (canTx(0, 0x6B4, false, cTxOn, 1) != CAN_OK) {
               Serial.print("Error sending message\n");
             }
@@ -91,12 +114,14 @@ void loop()
             Serial.print("Set source high\n");
             digitalWrite(PRECHARGE_RELAY_PIN, HIGH);
             Serial.print("Set precharge high\n");
+            digitalWrite(AUX_RELAY_PIN, HIGH);
+            Serial.print("Set auxiliary relay high\n");
             if (canTx(0, 0x6B4, false, cTxOn, 1) != CAN_OK) {
               Serial.print("Error sending message\n");
             }
           }
           Serial.print("Discharge enabled");
-          Serial.print("\n"); 
+          Serial.print("\n");
         }
         else
         {
@@ -104,16 +129,20 @@ void loop()
           digitalWrite(SOURCE_RELAY_PIN, HIGH);
           Serial.print("Set digital high\n");
           digitalWrite(PRECHARGE_RELAY_PIN, HIGH);
+          Serial.print("Set auxiliary relay high\n");
+          digitalWrite(AUX_RELAY_PIN, HIGH);
         }
       }
     } // end if
 
-    if (timer - lastCanFrame > CAN_THRESHOLD) 
+    if (timer - lastCanFrame > CAN_THRESHOLD)
     {
       Serial.print("Set source high\n");
       digitalWrite(SOURCE_RELAY_PIN, HIGH);
       Serial.print("Set digital high\n");
       digitalWrite(PRECHARGE_RELAY_PIN, HIGH);
+      Serial.print("Set auxiliary relay high\n");
+      digitalWrite(AUX_RELAY_PIN, HIGH);
     }
 
   }// end while
